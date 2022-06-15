f4f6972a0b13d494b01b5d13bc6ec49574137493
jiacian0609 <jiacian.yang@gmail.com>
2022-06-15 16:21:09 +0800

    feat: get group

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const db = require('../db');  
 
-/* GET card list. */
+/* GET card list */
 router.get('/', async function(req, res, next) {
   try {
     await db.connect();
@@ -19,6 +19,30 @@ router.get('/', async function(req, res, next) {
     res.status(200).send({
       message: 'successfully get card list',
       cards: cardList
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    db.close();
+  }
+});
+
+/* GET group */
+router.get('/group', async function(req, res, next) {
+  try {
+    await db.connect();
+    console.log('Connection Success');
+
+    const database = db.db('HanDOL');
+    const groups = database.collection('groups');
+
+    // const query = { group: group, member: member, album: album, version: version };
+    // const options = { projection: { _id: 1 } };
+    const groupList = await groups.find().toArray();
+
+    res.status(200).send({
+      message: 'successfully get group list',
+      cards: groupList
     });
   } catch (err) {
     console.log(err);
