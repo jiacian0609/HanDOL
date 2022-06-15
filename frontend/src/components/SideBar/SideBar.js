f4a866eb3dedb2a8392f47e5488e990a3902dd34
jiacian0609 <jiacian.yang@gmail.com>
2022-06-15 16:08:29 +0800

    feat: link

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -14,28 +14,38 @@ export default function SideBar({showSideBar, setShowSideBar}) {
                     <SideBarButton src='bars'/>
                     <SideBarTitle show={showSideBar}>HanDOL</SideBarTitle>
                 </SideBarField>
-                <SideBarField>
-                    <SideBarButton src='home' />
-                    <SideBarText show={showSideBar}>Home</SideBarText>
-                </SideBarField>
-                <SideBarField>
-                    <SideBarButton src='list' />
-                    <SideBarText show={showSideBar}>Card List</SideBarText>
-                </SideBarField>
-                <SideBarField>
-                    <SideBarButton src='template' />
-                    <SideBarText show={showSideBar}>Template</SideBarText>
-                </SideBarField>
-                <SideBarField>
-                    <SideBarButton src='profile' />
-                    <SideBarText show={showSideBar}>Profile</SideBarText>
-                </SideBarField>
+                <Link to='home' style={{ textDecoration: 'none', color: 'inherit'}}>
+                    <SideBarField>
+                        <SideBarButton src='home' />
+                        <SideBarText show={showSideBar}>Home</SideBarText>
+                    </SideBarField>
+                </Link>
+                <Link to='/cardlist' style={{ textDecoration: 'none', color: 'inherit'}}>
+                    <SideBarField>
+                        <SideBarButton src='list' />
+                        <SideBarText show={showSideBar}>Card List</SideBarText>
+                    </SideBarField>
+                </Link>
+                <Link to='/template' style={{ textDecoration: 'none', color: 'inherit'}}>
+                    <SideBarField>
+                        <SideBarButton src='template' />
+                        <SideBarText show={showSideBar}>Template</SideBarText>
+                    </SideBarField>
+                </Link>
+                <Link to='/profile' style={{ textDecoration: 'none', color: 'inherit'}}>
+                    <SideBarField>
+                        <SideBarButton src='profile' />
+                        <SideBarText show={showSideBar}>Profile</SideBarText>
+                    </SideBarField>
+                </Link>
             </SideBarTopButtons>
             <SideBarDownButtons>
-                <SideBarField>
-                    <SideBarButton src='feedback' />
-                    <SideBarText show={showSideBar}>Feedback</SideBarText>
-                </SideBarField>
+                <Link to='/feedback' style={{ textDecoration: 'none', color: 'inherit'}}>
+                    <SideBarField>
+                        <SideBarButton src='feedback' />
+                        <SideBarText show={showSideBar}>Feedback</SideBarText>
+                    </SideBarField>
+                </Link>
                 <SideBarField onClick={() => handleSignOut()}>
                     <SideBarButton src='signout' />
                     <SideBarText show={showSideBar}>Sign Out</SideBarText>
