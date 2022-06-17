d51a907d62e336254ac0bc99de1aea28a9ab55ad
jiacian0609 <jiacian.yang@gmail.com>
2022-06-17 22:03:07 +0800

    style: layout

--- frontend/src/pages/layout/layout-style.js
+++ frontend/src/pages/layout/layout-style.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const LayoutWrapper = styled.div `
     width: 100%;
     height: 100%;
-`
 
-export const LayoutContent = styled.div `
-    width: 100%;
-    height: 100%;
-    padding-left: 300px;
+    padding-left: 80px;
+
+    position: absolute;
+
+    background-color: #FFFBEF;
 `
