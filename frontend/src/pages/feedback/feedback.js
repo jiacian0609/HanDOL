f12ad5d954a8d766550ee86457c53bed49bcb6b8
jiacian0609 <jiacian.yang@gmail.com>
2022-06-19 16:24:31 +0800

    rm: console.log

--- frontend/src/pages/feedback/feedback.js
+++ frontend/src/pages/feedback/feedback.js
@@ -24,7 +24,7 @@ export default function Feedback() {
 			}
 		})
         .then( (res) => {
-            console.log(res.data);
+            // console.log(res.data);
             window.alert(res.data.message);
 		})
 		.catch( (err) => {
