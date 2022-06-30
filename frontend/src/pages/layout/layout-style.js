b1690059c609b64b28b3d857369250e3ab47d049
jiacian0609 <79597797+jiacian0609@users.noreply.github.com>
2022-07-01 00:47:47 +0800

    feat: profile page

--- frontend/src/pages/layout/layout-style.js
+++ frontend/src/pages/layout/layout-style.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const LayoutWrapper = styled.div `
-    width: 100%;
-    height: 100%;
+    min-width: 100%;
+    min-height: 100%;
 
     padding-left: 80px;
 
